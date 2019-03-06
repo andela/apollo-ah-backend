e7a9e3d9fa24afbb25a3edb1f2984d2a95538da9
Harry Dbrandy <devdbrandy@gmail.com>
2019-03-06 09:50:44 +0100

    feature(authentication): implement user authentication with jwt
    - Added endpoints for signup and login
    - Added controller for handling authentication logic
    - Added authenticate middleware to handle user token verification
    - Write test against implementation
    - Added swagger documentation
    - Updated `Procfile` to temporarily run command `npm run start:dev`
    on heroku
    - Improved `coverage` script command [npmPackage]
    - Added `mocha` and `mocha:test` commands for separation of concern
    - Removed redundant codes and files
    
    [Finishes #164198179]

---
+++ routes/users.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import UsersController from '../controllers/users';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   post:
+ *     tags:
+ *       - register
+ *     description: Register a new user account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User's email
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully registered
+ *         schema:
+ *           $ref: '#/definitions/User'
+ */
+router.post('/', UsersController.register);
+
+/**
+ * @swagger
+ * /api/v1/users/login:
+ *   post:
+ *     tags:
+ *       - login
+ *     description: Login into user account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User's email
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully logged in
+ *         schema:
+ *           $ref: '#/definitions/User'
+ */
+router.post('/login', UsersController.login);
+
+export default router;
